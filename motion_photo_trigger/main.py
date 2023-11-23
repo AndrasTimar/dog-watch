a75ffd5c90a7dbacc9a2f2d8086d616e316bbab1
andrastimar <andrastimardev@gmail.com>
2023-11-23 17:39:31 +0100

    button

--- motion_photo_trigger/main.py
+++ motion_photo_trigger/main.py
@@ -1,16 +1,28 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import os
+from threading import Thread
 
 
 hostName = "raspberrypi.local"
 serverPort = 8080
 
+def takePhoto():
+    print("Taking photo")
+    os.system("libcamera-still -o ./photo_server/public/smaller.jpg --hflip --vflip")
+
+
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_POST(self):
         self.send_response(200)
-        print("generating image")
-        os.system("libcamera-still -o ../photo_server/public/smaller.jpg --hflip --vflip")
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        message = "Hello, World! Here is a POST response"
+        self.wfile.write(bytes(message, "utf8"))
+        Thread(target=takePhoto).start()
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        BaseHTTPRequestHandler.end_headers(self)
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
--- photo_server/src/App.tsx
+++ photo_server/src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = async () => {
+
+    try {
+      const response = await fetch('http://raspberrypi.local:8080', {
+        method: 'POST',
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      } else {
+        console.log("SUCCESS");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="App">
       <header>
-	<img className="main-image" src="/smaller.jpg" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	    <img className="main-image" src="/smaller.jpg"/>
+      <button  type="button" value="Click Me" onClick={handleClick} >Refresh</button>
       </header>
     </div>
   );
