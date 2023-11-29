01263ef98182a2af563e83349b748b82a613c96e
andrastimar <andrastimardev@gmail.com>
2023-11-29 16:58:46 +0100

    remove unused

--- dog_watch_server/rabbitmq_sender.py
+++ dog_watch_server/rabbitmq_sender.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 import pika
-#movement.started
-#movement
-#any message
+
 def send_to_topic(topic, exchange, message):
 	connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 	channel = connection.channel()
@@ -10,12 +8,3 @@ def send_to_topic(topic, exchange, message):
 	channel.basic_publish(exchange=exchange, routing_key=topic, body=message)
 	print(" [x] Sent: " + str(message))
 	connection.close()
-if __name__ == '__main__':
-	try:
-		send("A MESSAGE")
-	except KeyboardInterrupt:
-		print('Interrupted')
-		try:
-			sys.exit(0)
-		except SystemExit:
-			os._exit(0)
