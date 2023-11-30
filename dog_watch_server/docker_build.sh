f10077c61d6044e6786f4e44692f24b82863f3e1
Andras Timar <andrastimardev@gmail.com>
2023-11-30 17:30:36 +0100

    dog(motion) detector service

--- dog_watch_server/docker_build.sh
+++ dog_watch_server/docker_build.sh
@@ -1,2 +1,3 @@
 sudo docker image build -t camera_service:latest -f ./camera_service/Dockerfile .
 sudo docker image build -t server:latest -f ./web_server/Dockerfile .
+sudo docker image build -t dog_detector:latest -f ./dog_detector/Dockerfile .
